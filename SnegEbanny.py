51388f040330ef8111aaaa153a093608f03df6f6
DeathlyKlimm <88938748+DeathlyKlimm@users.noreply.github.com>
2021-08-14 18:30:41 +0300

    Add files via upload

---
+++ SnegEbanny.py
@@ -0,0 +1,31 @@
+from .. import loader 
+from asyncio import sleep  
+ 
+def register(cb): 
+ cb(SnegMod())  
+  
+class SnegMod(loader.Module): 
+ """Снег""" 
+ strings = {'name': 'ВикаЛох'} 
+         
+ async def snegcmd(self, message): 
+  """Используй .sneg""" 
+  await message.edit('☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁️\n\n\n\n\n\n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️') 
+  await sleep(0.75) 
+  await message.edit('☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁️\n    ❄️    ❄️     ❄️     ❄️     ❄️   ❄️\n\n\n\n\n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️') 
+  await sleep(0.75) 
+  await message.edit('☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁️\n    ❄️    ❄️     ❄️     ❄️     ❄️   ❄️\n❄️    ❄️    ❄️    ❄️    ❄️    ❄️       \n\n\n\n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️') 
+  await sleep(0.75) 
+  await message.edit(' ☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁️\n   ❄️    ❄️     ❄️     ❄️     ❄️   ❄️\n❄️    ❄️    ❄️    ❄️    ❄️    ❄️       \n    ❄️    ❄️    ❄️    ❄️    ❄️    ❄️     \n\n\n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️') 
+  await sleep(0.75) 
+  await message.edit('☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁️\n    🔥    🔥     🔥     ❄🔥     ❄️🔥   ❄️\n❄️    ❄️🔥    ❄️🔥    ❄️🔥    🔥❄️    🔥       \n    🔥❄️    🔥    🔥    ❄️    ❄️    ❄️     \n❄️    ❄️    ❄️    ❄️    ❄️    ❄️     \n\n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️') 
+  await sleep(0.75) 
+  await message.edit('☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁️\n    🔥    🔥     🔥     🔥     🔥   🔥\n🔥    🔥    🔥    🔥    🔥    🔥       \n    🔥    ❄️    ❄️    ❄️    ❄️    🔥     \n❄️    ❄️    ❄️    ❄️    ❄️    ❄️     \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️ \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️')  
+  await sleep(1.25) 
+  await message.edit('☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁️\n\n❄️    ❄️    ❄️    ❄️    ❄️    ❄️       \n    ❄️    ❄️    ❄️    ❄️    ❄️    ❄️     \n❄️    ❄️    ❄️    ❄️    ❄️    ❄️     \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️  \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️') 
+  await sleep(0.75) 
+  await message.edit('☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁️\n\n\n    ❄️    ❄️    ❄️    ❄️    ❄️    ❄️     \n❄️    ❄️    ❄️    🔥    ❄️    ❄️     \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️ \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️') 
+  await sleep(0.75) 
+  await message.edit('☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁️\n\n\n\n❄️    ❄️    ❄️    ❄️    ❄️    ❄️     \n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️ \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️') 
+  await sleep(0.75) 
+  await message.edit('☁️🌨☁️🌨☁️🌨☁️🌨☁️🌨☁️\n\n\n\n\n  ❄️      ❄️    ❄️  ❄️      ❄️  ❄️ \n⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️☃️⛄️')
